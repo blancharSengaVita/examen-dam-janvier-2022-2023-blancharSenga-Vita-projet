4237e5b6442a290e9913826747cbc46b18f4d6d9
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2022-12-13 20:50:07 +0100

    builiding search input

--- lib/main.dart
+++ lib/main.dart
@@ -11,26 +11,37 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
             body: Padding(
-              padding:
-              const EdgeInsets.only(top: kDefaultWidth, bottom: kDefaultWidth),
-              child: Row(
-                children: [
-                  SizedBox(
-                    width: kDefaultWidth,
+      padding: const EdgeInsets.only(top: kDefaultWidth, bottom: kDefaultWidth),
+      child: Row(
+        children: [
+          const SizedBox(
+            width: kDefaultWidth,
+          ),
+          ClipRRect(
+              borderRadius: BorderRadius.circular(50.0),
+              child: const Image(
+                image: AssetImage('assets/pictures/profile.jpg'),
+                width: kDefaultWidth,
+                height: kDefaultWidth,
+              )),
+          const SizedBox(
+            width: 11,
+          ),
+          Expanded(
+              child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: kMargin / 2,
                   ),
-                  Container(
-                      child: ClipRRect(
-                          borderRadius: BorderRadius.circular(50.0),
-                          child: const Image(
-                            image: AssetImage('assets/pictures/profile.jpg'),
-                            width: kDefaultWidth,
-                            height: kDefaultWidth,
-                          ))),
-                  SizedBox(
-                    width: 11,
-                  ),
-                ],
-              ),
-            )));
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                    border: Border.all(
+                      width: 2,
+                    ),
+                  )))
+        ],
+      ),
+    )));
   }
 }
