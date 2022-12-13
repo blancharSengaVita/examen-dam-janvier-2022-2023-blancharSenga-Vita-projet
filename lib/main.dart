2b0aeb7531ff803d0d86d97b3dd133f0335e6f84
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2022-12-13 20:14:47 +0100

    adding profile picture and font

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:exame/styles/constants.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,10 +8,29 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        
-      )
-    );
+    return MaterialApp(
+        home: Scaffold(
+            body: Padding(
+              padding:
+              const EdgeInsets.only(top: kDefaultWidth, bottom: kDefaultWidth),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: kDefaultWidth,
+                  ),
+                  Container(
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(50.0),
+                          child: const Image(
+                            image: AssetImage('assets/pictures/profile.jpg'),
+                            width: kDefaultWidth,
+                            height: kDefaultWidth,
+                          ))),
+                  SizedBox(
+                    width: 11,
+                  ),
+                ],
+              ),
+            )));
   }
 }
