185b64495ed9601f6718b066117f60821f0e40c5
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2022-12-14 19:22:59 +0100

    new try to use firebase

--- lib/routes/router.dart
+++
@@ -1,8 +0,0 @@
-import 'package:exame/routes/routes.dart';
-import 'package:flutter/material.dart';
-
-import '../screens/home.dart';
-
-Map<String, WidgetBuilder> router = {
-  kHomeRoute: (context) =>  const Home(),
-};
