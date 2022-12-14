15d0e71fd7234fc8106d52acc2d64912cd8aa5f3
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2022-12-14 12:15:56 +0100

    end of navbar

--- lib/main.dart
+++ lib/main.dart
@@ -13,52 +13,43 @@ class MyApp extends StatelessWidget {
             body: Padding(
       padding: const EdgeInsets.only(top: kDefaultWidth, bottom: kDefaultWidth),
       child: Row(
-        children: [
-          const SizedBox(
-            width: kMargin / 2,
-          ),
-          ClipRRect(
-              borderRadius: BorderRadius.circular(50.0),
-              child: const Image(
-                image: AssetImage('assets/pictures/profile.jpg'),
-                width: kDefaultWidth,
-                height: kDefaultWidth,
-              )),
-          const SizedBox(
-            width: 11,
-          ),
-          Expanded(
+          children: [
+            const SizedBox(
+              width: kMargin,
+            ),
+            ClipRRect(
+                borderRadius: BorderRadius.circular(50.0),
+                child: const Image(
+                  image: AssetImage('assets/pictures/profile.jpg'),
+                  width: kDefaultWidth,
+                  height: kDefaultWidth,
+                )),
+            const SizedBox(
+              width: 11,
+            ),
+            Expanded(
               child: Container(
-            height: 50,
-            padding: const EdgeInsets.symmetric(
-              horizontal: kMargin / 2,
-            ),
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.all(
-                Radius.circular(25),
-              ),
-              border: Border.all(
-                width: 2,
-                color: Colors.black,
-              ),
-            ),
-            child: Row(
-              children: const  [
-                Text(
-                  "Rechercher une note",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 17.0,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.grey,
-                    decoration: TextDecoration.none,
+                height: 50,
+                padding: const EdgeInsets.symmetric(
+                  horizontal: kMargin / 2,
+                ),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'rechercher une note',
                   ),
                 ),
-              ],
+              ),
             ),
-          ))
-        ],
-      ),
+            const SizedBox(
+              width: 11,
+            ),
+            const Icon(Icons.more_vert),
+            const SizedBox(
+              width: kMargin,
+            )
+          ],
+          ),
     )));
   }
 }
