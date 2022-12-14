ff532969dca64606987b7c89cc0430dac619b5c4
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2022-12-14 15:26:55 +0100

    firebase added

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -7,9 +7,14 @@ import '../partials/navbar/menu.dart';
 import '../partials/navbar/search_input.dart';
 import '../styles/constants.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
@@ -21,7 +26,6 @@ class Home extends StatelessWidget {
               // AddNoteButton(),
             ],
           ),
-
         ));
   }
 }
