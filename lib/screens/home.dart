a51bf223252d546e0bc6157d7b04fa2b85e399ea
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2022-12-14 22:47:56 +0100

    Making Galerie of note

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,16 +12,138 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
         body: Padding(
-          padding: const EdgeInsets.only(top: kDefaultWidth, bottom: kDefaultWidth),
-          child: Column(
+      padding: const EdgeInsets.only(top: kMargin * 3, bottom: kMargin),
+      child: Column(
+        children: [
+          const NavBar(),
+          const SizedBox(
+            height: 25,
+          ),
+          Row(
             children: const [
-              NavBar(),
-              // AddNoteButton(),
+              SizedBox(
+                width: 20,
+              ),
+               Text(
+                "Toutes les notes",
+                style: kTitleStyle,
+                textAlign: TextAlign.left,
+              ),
             ],
           ),
-
-        ));
+          GridView.count(
+            padding: const EdgeInsets.symmetric(vertical: kMargin/1.33, horizontal: kMargin),
+            crossAxisCount: 2,
+            mainAxisSpacing: 15,
+            crossAxisSpacing: 10,
+            shrinkWrap: true,
+            // physics: NeverScrollableScrollPhysics(),
+            children: [
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 23, horizontal: 20),
+                // margin: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: Colors.white,
+                    border: Border.all(
+                      style: BorderStyle.solid,
+                      width: 2,
+                    )),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      "note_title",
+                      style: kNoteTitleStyle,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text("note_content", style: kMainTextStyle)
+                  ],
+                ),
+              ),
+              Container(
+                padding:
+                const EdgeInsets.symmetric(vertical: 23, horizontal: 20),
+                // margin: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: Colors.white,
+                    border: Border.all(
+                      style: BorderStyle.solid,
+                      width: 2,
+                    )),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      "note_title",
+                      style: kNoteTitleStyle,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text("note_content", style: kMainTextStyle)
+                  ],
+                ),
+              ),
+              Container(
+                padding:
+                const EdgeInsets.symmetric(vertical: 23, horizontal: 20),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: Colors.white,
+                    border: Border.all(
+                      style: BorderStyle.solid,
+                      width: 2,
+                    )),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      "note_title",
+                      style: kNoteTitleStyle,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text("note_content", style: kMainTextStyle)
+                  ],
+                ),
+              ),
+              Container(
+                padding:
+                const EdgeInsets.symmetric(vertical: 23, horizontal: 20),
+                // margin: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: Colors.white,
+                    border: Border.all(
+                      style: BorderStyle.solid,
+                      width: 2,
+                    )),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      "note_title",
+                      style: kNoteTitleStyle,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text("note_content", style: kMainTextStyle)
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    ));
   }
 }
