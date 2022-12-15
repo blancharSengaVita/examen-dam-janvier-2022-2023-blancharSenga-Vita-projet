1f64de4ca3ee2e74480a0f6912a69acf0c231082
blancharSengaVita <blanchar.senga-vita@student.hepl.be>
2022-12-15 17:42:08 +0100

    add Typing on editor page

--- lib/partials/note_card.dart
+++ lib/partials/note_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../screens/note_editor.dart';
 import '../styles/constants.dart';
 
 class NoteCard extends StatelessWidget {
@@ -10,30 +11,39 @@ class NoteCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 23, horizontal: 20),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(25),
-          color: Colors.white,
-          border: Border.all(
-            style: BorderStyle.solid,
-            width: 2,
-          )),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            title,
-            style: kCardNoteTitleStyle,
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const NoteEditor(),
           ),
-          const SizedBox(
-            height: 10,
-          ),
-          Text(
-              content,
-              style: kCardNoteContentTextStyle
-          )
-        ],
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 23, horizontal: 20),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(25),
+            color: Colors.white,
+            border: Border.all(
+              style: BorderStyle.solid,
+              width: 2,
+            )),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: kCardNoteTitleStyle,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+                content,
+                style: kCardNoteContentTextStyle
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/screens/note_editor.dart
+++ lib/screens/note_editor.dart
@@ -1,6 +1,8 @@
 import 'package:exame/styles/constants.dart';
 import 'package:flutter/material.dart';
 
+import 'home.dart';
+
 class NoteEditor extends StatefulWidget {
   const NoteEditor({Key? key}) : super(key: key);
 
@@ -9,42 +11,58 @@ class NoteEditor extends StatefulWidget {
 }
 
 class _NoteEditorState extends State<NoteEditor> {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _mainController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.all(kMargin),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children:  [
-              Container(
-                margin: const EdgeInsets.only(bottom : kMargin),
-                child : Row(
-                  children: [
-                    const Icon(Icons.arrow_back_ios, size: 30),
-                    Container(
-
-                    )
-                  ],
+        body: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.all(kMargin),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(bottom: kMargin),
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => const Home(),
+                              ),
+                            );
+                          },
+                          child: const Icon(Icons.arrow_back_ios, size: 30)),
+                      Container()
+                    ],
+                  ),
                 ),
-              ),
-              const Text(
-                "title",
-                style: kBigTitleStyle,
-              ),
-
-              const SizedBox(
-                height: 10,
-              ),
-              const Text(
-                  "content",
-                  style: kMainTextStyle
-              )
-            ],
+                TextField(
+                    controller: _titleController,
+                    decoration: const InputDecoration(
+                      hintText: "Title",
+                      border: InputBorder.none,
+                    ),
+                    style: kBigTitleStyle
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                    keyboardType: Text,
+                    controller: _mainController,
+                    decoration: const InputDecoration(
+                      hintText: "Note",
+                      border: InputBorder.none,
+                    ),
+                    style: kMainTextStyle
+                ),
+              ],
+            ),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
